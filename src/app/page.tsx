f9040f9abc61dde1b54447bf52d4a464424ab2d2
Matheus Mattos Avelino <114790698+MatheusMA678@users.noreply.github.com>
2023-05-11 03:25:33 +0000

    ajustando o tamanho das coisas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,28 @@
+"use client";
+
 import { Avatar } from "@/components/Avatar";
+import React from "react";
+import { motion } from "framer-motion";
 
 export default function Home() {
   return (
     <main className="p-8 h-screen flex items-center justify-center bg-gray-200">
-      <section className="flex flex-col justify-between w-[800px] h-[600px] rounded-3xl bg-white shadow-lg px-14 py-6 text-[#372D3B]">
-        <div className="w-[200px] h-4 rounded-xl bg-[#E5D8EA] self-center"></div>
+      <motion.section
+        drag
+        className={`flex flex-col justify-between w-[600px] h-[400px] rounded-3xl bg-white shadow-lg px-14 py-6 text-[#372D3B]`}
+      >
+        <div className="w-[150px] h-2 rounded-xl bg-[#E5D8EA] self-center"></div>
         <section>
-          <h1 className="font-bold text-4xl">
+          <h1 className="font-bold text-2xl">
             Encontre <span className="text-[#864293]">Boris</span> no local de
             partida
           </h1>
-          <span className="text-2xl font-medium">
+          <span className="text-sm font-medium">
             Chega em 3 minutos (800 metros)
           </span>
         </section>
         <Avatar />
-      </section>
+      </motion.section>
     </main>
   );
 }
--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -6,9 +6,13 @@ import AvatarImage from "../assets/pessoa.png";
 
 export function Avatar() {
   return (
-    <div className="relative flex justify-center w-fit">
-      <Image src={AvatarImage} alt="Foto do motorista" />
-      <div className="absolute w-24 text-xl rounded-full bg-[#372D3B] text-[#FBF8FC] -bottom-4 flex items-center justify-between py-2 px-5">
+    <div className="relative flex justify-center w-fit h-24">
+      <Image
+        src={AvatarImage}
+        alt="Foto do motorista"
+        className="w-20 h-20 rounded-full"
+      />
+      <div className="absolute text-sm rounded-full bg-[#372D3B] text-[#FBF8FC] bottom-0 flex items-center gap-1 py-1 px-2">
         <FaStar />
         <span>5.0</span>
       </div>
